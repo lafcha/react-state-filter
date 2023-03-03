d90657f8e5a6ea1322acab75fc9bbc682e89007d
clafarge <clafarge@numerian.fr>
2023-03-03 14:31:30 +0100

    Initial commit and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [items, setItems] = useState([]);
+  //On utilise le hook useRef pour récupérer les données de l'input
+  const inputRef = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    //On peut récupérer la valeur de l'input 
+    const value = inputRef.current.value;
+    if (value === "") return
+    setItems(prev => {
+      return [...prev, value]
+    })
+    //Une fois la valeur stockée dans le state, on peut vider l'input
+    inputRef.current.value="";
+  }
+
+    return (
+        <>
+            Search: 
+            <input type="search" />
+        <form onSubmit={onSubmit}>
+          New Item: 
+          {/* On ajoute le hook en ref de l'input */}
+          <input type="text" ref={inputRef} />
+          <button type="submit">Add</button>
+        </form>
+        <h3>Items: </h3>
+        {items.map(item => (
+          <div>{item}</div>
+        ))}
+        </>
+    );
 }
 
 export default App;
